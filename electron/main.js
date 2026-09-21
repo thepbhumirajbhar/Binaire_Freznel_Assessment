9c0da93e8302cbc90563a0011cb3c091147dbbf4
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-09-21 22:23:07 +0530

    electron: setup basic main process window"

---
+++ electron/main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    title: "Panoramic Stitcher",
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  win.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
